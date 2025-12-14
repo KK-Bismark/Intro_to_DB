b6a50288fd04994b0ad43944a58e012a03bc3792
KK-Bismark <kbismarkkwaku@gmail.com>
2025-12-14 19:23:50 +0000

    Solution to task0

---
+++ alx_book_store.sql
@@ -0,0 +1,12 @@
+-- Designing a MySQL database for an online bookstore.
+-- The database should store information about books,
+-- authors, customers, orders, and order details.
+
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE Books (
+	book_id INT PRIMARY KEY,
+	title VARCHAR(130),
+	author_id INT,
+	price DOUBLE,
+	publication_date DATE );
