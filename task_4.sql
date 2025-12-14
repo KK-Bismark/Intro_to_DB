5a561e3dd8642e9e4c175056eba9229966b14628
KK-Bismark <kbismarkkwaku@gmail.com>
2025-12-14 23:03:53 +0000

    Solution to task 4

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+-- A script that prints the full description of the table books from the
+-- database alx_book_store in your MySQL server.
+
+SELECT
+	COLUMN_NAME AS 'Column',
+	COLUMN_TYPE AS 'Type',
+	COLUMN_KEY AS 'Key',
+	COLUMN_DEFAULT AS 'Default',
+	EXTRA AS 'Extra'
+FROM
+	INFORMATION_SCHEMA.COLUMNS
+WHERE
+	TABLE_SCHEMA = DATABASE()
+	AND TABLE_NAME = 'books'
+ORDER BY
+	ORDINAL_POSITION;
+
