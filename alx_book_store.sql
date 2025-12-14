f98c611b900122c5721b8606b717fe03a50e785d
KK-Bismark <kbismarkkwaku@gmail.com>
2025-12-14 19:55:23 +0000

    Provided the remaining lines of codes for creating the books, customers, orders and order details table.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -10,9 +10,43 @@ CREATE TABLE Authors ( author_id INT PRIMARY KEY,
        author_name VARCHAR(215)
 );
 
+-- Creates the Books table.
 CREATE TABLE Books (
 	book_id INT PRIMARY KEY,
 	title VARCHAR(130),
 	author_id INT,
 	price DOUBLE,
-	publication_date DATE );
+	publication_date DATE
+);
+
+-- Creates the Customers table.
+CREATE TABLE Customers (
+	customer_id INT PRIMARY KEY,
+	customer_name VARCHAR(215),
+	email VARCHAR(215),
+	address TEXT
+);
+
+-- Creates the Orders table.
+CREATE TABLE Orders (
+	order_id INT PRIMARY KEY,
+	customer_id INT,
+	order_date DATE,
+	CONSTRAINT foreignkey_orders_customers
+		FOREIGN KEY (customer_id)
+		REFERENCES Customers(customer_id)
+);
+
+-- Creates the Order Details table.
+CREATE TABLE Order_Details (
+	orderdetailid INT PRIMARY KEY,
+	order_id INT,
+	book_id INT,
+	quantity DOUBLE,
+	CONSTRAINT foreignkey_orderdetails_orders
+		FOREIGN KEY (order_id)
+		REFERENCES Orders(order_id),
+	CONSTRAINT foreignkey_orderdetails_books
+		FOREIGN KEY (book_id)
+		REFERENCES Books(book_id)
+);
