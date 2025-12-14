d1df94fe3179e2037a3a9280d8630f428c461e94
KK-Bismark <kbismarkkwaku@gmail.com>
2025-12-14 23:11:39 +0000

    Corrected the wrong spelling of the table.

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 -- A script that inserts a single row in the table Customer.
 
-INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
