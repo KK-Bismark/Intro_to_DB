1ef83e84ce0bcb95164a3f22c82aba253de1dc43
KK-Bismark <kbismarkkwaku@gmail.com>
2025-12-14 19:57:14 +0000

    Fixed the error within the line for creating the orders table.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,9 +32,7 @@ CREATE TABLE Orders (
 	order_id INT PRIMARY KEY,
 	customer_id INT,
 	order_date DATE,
-	CONSTRAINT foreignkey_orders_customers
-		FOREIGN KEY (customer_id)
-		REFERENCES Customers(customer_id)
+	CONSTRAINT foreignkey_orders_customers FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Creates the Order Details table.
